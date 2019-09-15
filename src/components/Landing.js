4dc8747593449280726552bb5bce444d4a661116
moniet <moniet.sawhney@gmail.com>
2019-09-16 01:57:00 +0530

    added greeting section in landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,3 +1,59 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import { colors } from '../utils/theme'
+import Button from './Button'
 
-export default () => <div></div>
+const Intro = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  top: 0;
+  left: 5vw;
+  margin: 0 auto;
+  height: 100vh;
+  width: 90vw;
+`
+
+const Greeting = styled.div`
+  display: block;
+  flex-basis: 40vw;
+
+  h1 {
+    font-weight: 500;
+    margin-bottom: 1.5rem;
+  }
+
+  h1,
+  p {
+    color: ${colors.text};
+  }
+`
+
+const Artwork = styled.div`
+  position: relative;
+  display: block;
+  flex-basis: 50vw;
+`
+
+export default () => (
+  <section>
+    <Intro>
+      <Greeting>
+        <h1>Hi There, I'm Moniet, a Full Stack Developer.</h1>
+        <p>
+          I build applications super quick with the power of Ruby on Rails and
+          supercharge the front end with React <mark></mark>
+        </p>
+        <p>
+          Having a background, I am enabled as a creative thinker have an
+          immense passion when it comes to the subtleties of design.
+        </p>
+        <Button>
+          <a href='/case-studies'>See what I've Made</a>
+        </Button>
+      </Greeting>
+      <Artwork></Artwork>
+    </Intro>
+  </section>
+)
